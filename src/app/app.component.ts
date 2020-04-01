de5e3c830372116e169e3dc4dda1e5afebafb77c
ismail salmi <ismail.man.2013@gmail.com>
2020-04-01 03:40:07 +0200

    add new ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'myApp';
-}
+  listCountry: any = []
+  isScrolled :boolean = false
+  constructor(private http: HttpClient) { }
+
+  // Api Read
+  ngOnInit() {
+
+    this.http.get('https://coronavirus-19-api.herokuapp.com/countries/')
+      .subscribe(res => {
+        this.listCountry = res
+      },
+        (err) => {
+          alert('Failed loading json data');
+        });
+  }
+
+  //scrol listening
+  @HostListener('window:scroll', ['$event']) onWindowScroll(e) {
+    let scroll = e.target['scrollingElement'].scrollTop
+    console.log(scroll)
+    if (scroll > 100){
+     this.isScrolled = true
+    }else{
+      this.isScrolled = false
+    }
+
+  }
+    //back to top 
+    topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      // window.scrollTo(0, 0)
+    }
+
+
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Angular http requests
+import { HttpClientModule } from '@angular/common/http'
+// Angular material forms
+import { FormsModule }   from '@angular/forms';
+// Angular material button
+import {MatButtonModule} from '@angular/material/button'; 
+// Angular material icon
+import {MatIconModule} from '@angular/material/icon'; 
+// Sort Items 
+import { NgpSortModule } from "ngp-sort-pipe";
 
 @NgModule({
   declarations: [
@@ -10,7 +21,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    NgpSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
